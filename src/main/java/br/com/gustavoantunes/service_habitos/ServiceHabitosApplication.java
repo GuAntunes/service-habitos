d013ad936d64ad05f9373d6bfbdab0b37ae47594
Gustavo Antunes <gas.antunes84@gmail.com>
2020-04-02 21:16:39 -0300

    First Commit

---
+++ src/main/java/br/com/gustavoantunes/service_habitos/ServiceHabitosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavoantunes.service_habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHabitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavoantunes/service_habitos/ServiceHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoantunes.service_habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
